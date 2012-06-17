8b3483cf6d9670fac805172a2436fee966d30415
Lejdborg <christoffer.lejdborg@gmail.com>
2012-06-17 19:38:56 +0200

    Setup new basic project structure

---
+++ NMSSH/NMSSH/NMSSH.h
@@ -0,0 +1,13 @@
+//
+//  NMSSH.h
+//  NMSSH
+//
+//  Created by Christoffer Lejdborg on 2012-06-17.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NMSSH : NSObject
+
+@end
