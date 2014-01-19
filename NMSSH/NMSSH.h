2d5c1a0d74b34dd1e5c5a2d441c8f65b609e7074
Tommaso Madonia <frugghi@gmail.com>
2014-01-19 02:05:26 +0100

    NMSSH.h clean up

--- NMSSH/NMSSH.h
+++ NMSSH/NMSSH.h
@@ -1,13 +1,6 @@
 #import "libssh2.h"
 #import "libssh2_sftp.h"
 
-#import <CoreFoundation/CoreFoundation.h>
-#import <netinet/in.h>
-#import <sys/socket.h>
-#import <arpa/inet.h>
-
-#define kNMSSHBufferSize (0x4000)
-
 @class NMSSHSession, NMSSHChannel, NMSFTP, NMSFTPFile;
 
 #import "NMSSHSession.h"
