29b07fe44d77d72a30726892063ca3b3f5a65d58
Eric Chamberlain <eric.chamberlain@hotmail.com>
2014-04-03 22:05:34 -0700

    Added comments to all asynchronous functions. Corrected the comments to method parameters for copyContentsOfPath:toFileAtPath:

--- NMSSH/NMSFTP.h
+++ NMSSH/NMSFTP.h
@@ -61,10 +61,14 @@
 - (BOOL)moveItemAtPath:(NSString *)sourcePath toPath:(NSString *)destPath;
 
 /**
- Execute method moveItemAtPath:toPath: asynchronously.
+ Refer to moveItemAtPath:toPath:
+ 
+ This adds the ability to perform the operation asynchronously.
  
  @param sourcePath Item to move
  @param destPath Destination to move to
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
  */
 - (void)moveItemAtPath:(NSString *)sourcePath toPath:(NSString *)destPath
                success:(void (^)(BOOL success))success
@@ -84,42 +88,78 @@
  */
 - (BOOL)directoryExistsAtPath:(NSString *)path;
 
+/**
+ Refer to directoryExistsAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param path Path to check
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)directoryExistsAtPath:(NSString *)path
                       success:(void (^)(BOOL success))success
                       failure:(void (^)(NSError *error))failure;
 
 /**
- Create a directory at path
+ Create a directory at path.
 
  @param path Path to directory
  @returns Creation success
  */
 - (BOOL)createDirectoryAtPath:(NSString *)path;
 
+/**
+ Refer to createDirectoryAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param path Path to directory
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)createDirectoryAtPath:(NSString *)path
                       success:(void (^)(BOOL success))success
                       failure:(void (^)(NSError *error))failure;
 
 /**
- Remove directory at path
+ Remove directory at path.
 
  @param path Existing directory
  @returns Remove success
  */
 - (BOOL)removeDirectoryAtPath:(NSString *)path;
 
+/**
+ Refer to removeDirectoryAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param path Existing directory
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)removeDirectoryAtPath:(NSString *)path
                       success:(void (^)(BOOL success))success
                       failure:(void (^)(NSError *error))failure;
 
 /**
- Get a list of files for a directory path
+ Get a list of files for a directory path.
 
  @param path Existing directory to list items from
  @returns List of relative paths
  */
 - (NSArray *)contentsOfDirectoryAtPath:(NSString *)path;
 
+/**
+ Refer to contentsOfDirectoryAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param path Existing directory
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)contentsOfDirectoryAtPath:(NSString *)path
                           success:(void (^)(NSArray *contents))success
                           failure:(void (^)(NSError *error))failure;
@@ -136,6 +176,15 @@
  */
 - (NMSFTPFile *)infoForFileAtPath:(NSString *)path;
 
+/**
+ Refer to infoForFileAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param path An existing file path
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)infoForFileAtPath:(NSString *)path
                   success:(void (^)(NMSFTPFile *file))success
                   failure:(void (^)(NSError *error))failure;
@@ -150,12 +199,21 @@
  */
 - (BOOL)fileExistsAtPath:(NSString *)path;
 
+/**
+ Refer to fileExistsAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param path Path to check
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)fileExistsAtPath:(NSString *)path
                  success:(void (^)(BOOL exists))success
                  failure:(void (^)(NSError *error))failure;
 
 /**
- Create a symbolic link
+ Create a symbolic link.
 
  @param linkPath Path that will be linked to
  @param destPath Path the link will be created at
@@ -163,30 +221,58 @@
  */
 - (BOOL)createSymbolicLinkAtPath:(NSString *)linkPath withDestinationPath:(NSString *)destPath;
 
+/**
+ Refer to createSymbolicLinkAtPath:withDestinationPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param linkPath Path that will be linked to
+ @param destPath Path the link will be created at
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)createSymbolicLinkAtPath:(NSString *)linkPath withDestinationPath:(NSString *)destPath
                          success:(void (^)(BOOL success))success
                          failure:(void (^)(NSError *error))failure;
 
 /**
- Remove file at path
+ Remove file at path.
 
  @param path Path to existing file
  @returns Remove success
  */
 - (BOOL)removeFileAtPath:(NSString *)path;
 
+/**
+ Refer to removeFileAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param path Path to existing file
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)removeFileAtPath:(NSString *)path
                  success:(void (^)(BOOL success))success
                  failure:(void (^)(NSError *error))failure;
 
 /**
- Read the contents of a file
+ Read the contents of a file.
 
  @param path An existing file path
  @returns File contents
  */
 - (NSData *)contentsAtPath:(NSString *)path;
 
+/**
+ Refer to contentsAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param path An existing file path
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)contentsAtPath:(NSString *)path
                success:(void (^)(NSData *contents))success
                failure:(void (^)(NSError *error))failure;
@@ -198,12 +284,23 @@
  
  @param path An existing file path
  @param progress Method called periodically with number of bytes downloaded and total file size.
-        Returns NO to abort. 
+        Returns NO to abort.
  @returns File contents
  */
 - (NSData *)contentsAtPath:(NSString *)path
                   progress:(BOOL (^)(NSUInteger got, NSUInteger totalBytes))progress;
 
+/**
+ Refer to contentsAtPath:progress:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param path An existing file path
+ @param progress Method called periodically with number of bytes downloaded and total file size.
+        Returns NO to abort.
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)contentsAtPath:(NSString *)path
               progress:(BOOL (^)(NSUInteger got, NSUInteger totalBytes))progress
                success:(void (^)(NSData *contents))success
@@ -220,6 +317,16 @@
  */
 - (BOOL)writeContents:(NSData *)contents toFileAtPath:(NSString *)path;
 
+/**
+ Refer to writeContents:toFileAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param contents Bytes to write
+ @param path File path to write bytes at
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)writeContents:(NSString *)path toFileAtPath:(NSString *)path
               success:(void (^)(BOOL success))success
               failure:(void (^)(NSError *error))failure;
@@ -237,6 +344,18 @@
  */
 - (BOOL)writeContents:(NSData *)contents toFileAtPath:(NSString *)path progress:(BOOL (^)(NSUInteger sent))progress;
 
+/**
+ Refer to writeContents:toFileAtPath:progress:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param contents Bytes to write
+ @param path File path to write bytes at
+ @param progress Method called periodically with number of bytes sent.
+        Returns NO to abort.
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)writeContents:(NSString *)path toFileAtPath:(NSString *)path
              progress:(BOOL (^)(NSUInteger sent))progress
               success:(void (^)(BOOL success))success
@@ -253,6 +372,16 @@
  */
 - (BOOL)writeFileAtPath:(NSString *)localPath toFileAtPath:(NSString *)path;
 
+/**
+ Refer to writeFileAtPath:toFileAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param localPath File path to read bytes at
+ @param path File path to write bytes at
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)writeFileAtPath:(NSString *)localPath toFileAtPath:(NSString *)path
                 success:(void (^)(BOOL success))success
                 failure:(void (^)(NSError *error))failure;
@@ -271,6 +400,18 @@
 - (BOOL)writeFileAtPath:(NSString *)localPath toFileAtPath:(NSString *)path
                progress:(BOOL (^)(NSUInteger sent))progress;
 
+/**
+ Refer to writeFileAtPath:toFileAtPath:progress:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param localPath File path to read bytes at
+ @param path File path to write bytes at
+ @param progress Method called periodically with number of bytes sent.
+        Returns NO to abort.
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)writeFileAtPath:(NSString *)localPath toFileAtPath:(NSString *)path
                progress:(BOOL (^)(NSUInteger sent))progress
                 success:(void (^)(BOOL success))success
@@ -287,6 +428,16 @@
  */
 - (BOOL)writeStream:(NSInputStream *)inputStream toFileAtPath:(NSString *)path;
 
+/**
+ Refer to writeStream:toFileAtPath:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param inputStream Stream to read bytes from
+ @param path File path to write bytes at
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)writeStream:(NSInputStream *)inputStream toFileAtPath:(NSString *)path
                 success:(void (^)(BOOL success))success
                 failure:(void (^)(NSError *error))failure;
@@ -304,6 +455,18 @@
  */
 - (BOOL)writeStream:(NSInputStream *)inputStream toFileAtPath:(NSString *)path progress:(BOOL (^)(NSUInteger sent))progress;
 
+/**
+ Refer to writeStream:toFileAtPath:progress:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param inputStream Stream to read bytes from
+ @param path File path to write bytes at
+ @param progress Method called periodically with number of bytes sent.
+        Returns NO to abort.
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)writeStream:(NSInputStream *)inputStream toFileAtPath:(NSString *)path
            progress:(BOOL (^)(NSUInteger sent))progress
             success:(void (^)(BOOL success))success
@@ -336,10 +499,25 @@
  
  @param fromPath Path to copy from
  @param toPath Path to copy to
+ @param progress Method called periodically with number of bytes sent.
+        Returns NO to abort.
+ @returns Write success
  */
 - (BOOL)copyContentsOfPath:(NSString *)fromPath toFileAtPath:(NSString *)toPath
                   progress:(BOOL (^)(NSUInteger copied, NSUInteger totalBytes))progress;
 
+/**
+ Refer to writeStream:toFileAtPath:progress:
+ 
+ This adds the ability to perform the operation asynchronously.
+ 
+ @param fromPath Path to copy from
+ @param toPath Path to copy to
+ @param progress Method called periodically with number of bytes sent.
+        Returns NO to abort.
+ @param success Method called when the process succeeds
+ @param failure Method called when the process fails
+ */
 - (void)copyContentsOfPath:(NSString *)fromPath toFileAtPath:(NSString *)toPath
                   progress:(BOOL (^)(NSUInteger copied, NSUInteger totalBytes))progress
                    success:(void (^)(BOOL success))success
