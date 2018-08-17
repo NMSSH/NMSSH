94624c90e357eda412e40c1a3d9dec1a1cb7479d
Tommaso Madonia <tommaso@madonia.me>
2018-08-17 01:36:11 +0100

    Change public key to nullable
    Fix #255

--- NMSSH/NMSSHSession.h
+++ NMSSH/NMSSHSession.h
@@ -230,7 +230,7 @@ typedef NS_ENUM(NSInteger, NMSSHKnownHostStatus) {
  @param password Password for encrypted private key
  @returns Authentication success
  */
-- (BOOL)authenticateByPublicKey:(nonnull NSString *)publicKey
+- (BOOL)authenticateByPublicKey:(nullable NSString *)publicKey
                      privateKey:(nonnull NSString *)privateKey
                     andPassword:(nullable NSString *)password;
 
@@ -244,7 +244,7 @@ typedef NS_ENUM(NSInteger, NMSSHKnownHostStatus) {
  @param password Password for encrypted private key
  @returns Authentication success
  */
-- (BOOL)authenticateByInMemoryPublicKey:(nonnull NSString *)publicKey
+- (BOOL)authenticateByInMemoryPublicKey:(nullable NSString *)publicKey
                              privateKey:(nonnull NSString *)privateKey
                             andPassword:(nullable NSString *)password;
 
